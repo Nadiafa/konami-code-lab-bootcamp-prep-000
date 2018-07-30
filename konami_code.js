a68aa05046950944a58fd9ba048db29173130234
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-07-30 14:20:25 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -10,37 +10,34 @@ const codes = [
   "b",
   "a"
 ];
-let index = 0;
+
+
 function init() {
   // your code here
   let index = 0;
-  const key = e.which;
   
-  if (key === codes[index]){
-    index++;
+  document.body.addEventListener('keydown', (e) => {
+    const key = e.key;
+    index = (codes[index] === key) ? ++index : 0
+    
     if (index === codes.length) {
       alert ('Well Done!');
       index = 0;
     }
-  } else {
-    index = 0;
-  }
-}
-
-
-function init() {
-  let idx = 0
-
-  document.body.addEventListener("keydown", (e) => {
-    const key = e.key
-    
-    idx = (codes[idx] === key) ? ++idx : 0
-    
-    if (idx === codes.length) {
-      window.alert("Hurray!");
-      idx = 0
-    }
-    
   });
-}
+} 
+
+// function init() {
+//   let idx = 0
+//   document.body.addEventListener("keydown", (e) => {
+//     const key = e.key
+//     idx = (codes[idx] === key) ? ++idx : 0
+    
+//     if (idx === codes.length) {
+//       window.alert("Hurray!");
+//       idx = 0
+//     }
+    
+//   });
+// }
 
