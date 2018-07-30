d28dc1f9b99bf9b177ce0a5492103e8d1dfc2802
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-07-30 14:13:44 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,20 @@ const codes = [
   "a"
 ];
 
-function init() {
+let index = 0;
+
+function init(e) {
   // your code here
+  const key = e.key;
+  
+  if (key === codes[index]){
+    index++;
+    if (index === codes.length) {
+      alert ('Well Done!');
+      index = 0;
+    }
+  } else {
+    index = 0;
+  }
+  
 }
